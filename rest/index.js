566bd079fbe9430aa0fb42cffe0e253f9776a130
pasukanbocil <ctian8787@gmail.com>
2024-04-02 13:58:47 +0700

    docs: :lock: MBAK M

--- rest/index.js
+++ rest/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get("/order", (req, res) => {
+  res.send("GET order response");
+});
+
+app.post("/order", (req, res) => {
+  res.send("POST order response");
+});
+
+
 app.listen(8080, () => {
   console.log("Server is running on: http://localhost:8080");
 });
