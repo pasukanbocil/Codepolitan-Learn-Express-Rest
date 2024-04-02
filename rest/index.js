458aad992daa8efef6b3175dd8c5fa968afcd0b1
pasukanbocil <ctian8787@gmail.com>
2024-04-02 14:32:46 +0700

    docs: :closed_lock_with_key: Add JSON and URL-encoded body parsing middleware

--- rest/index.js
+++ rest/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/order", (req, res) => {
   res.send("GET order response");
 });
 
 app.post("/order", (req, res) => {
-  res.send("POST order response");
+  const { item, qty } = req.body;
+  res.send(`Item: ${item}, Qty: ${qty}`);
 });
-
 
 app.listen(8080, () => {
   console.log("Server is running on: http://localhost:8080");
